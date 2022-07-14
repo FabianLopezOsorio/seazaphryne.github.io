4bc603a07baeaee10fc3228abe0235f63ae6dc72
Fabian <fabian960213@gmail.com>
2022-07-14 19:23:59 +0200

    init

---
+++ assets/home.3a4f47bc.js
@@ -0,0 +1,1 @@
+import{_ as c,c as a,b as e,r,o as s,g as _,h as m}from"./app.ba5393a9.js";const i={},f={class:"text-center text-gray-700 dark:text-gray-200"};function p(d,l){const o=_,t=r("RouterView"),n=m;return s(),a("main",f,[e(o),e(t),e(n)])}var u=c(i,[["render",p]]);export{u as default};
